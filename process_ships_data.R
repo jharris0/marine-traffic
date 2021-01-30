89853410e0724f19428f3152571a31935659d28c
Jesse Harris <jesserharris@icloud.com>
2021-01-30 13:15:27 -0700

    Added call to janitor::clean_names() to make all variable names lowercase

--- process_ships_data.R
+++ process_ships_data.R
@@ -1,27 +1,28 @@
-library(geosphere)
 library(readr)
 library(tidyr)
+library(janitor)
 library(dtplyr)
 library(dplyr, warn.conflicts = F)
+library(geosphere)
 
-ships_raw <- read_csv("data/ships.csv")
+ships_raw <- read_csv("data/ships.csv") %>% clean_names()
 
 ships_dt <- lazy_dt(ships_raw) %>%
-  group_by(SHIPNAME, PORT) %>%
-  arrange(DATETIME, .by_group = T) %>%
-  mutate(long_diff = LON - lag(LON),
-         lat_diff = LAT - lag(LAT),
-         prev_lon = lag(LON),
-         prev_lat = lag(LAT),
-         prev_datetime = lag(DATETIME)) %>%
+  group_by(shipname, port) %>%
+  arrange(datetime, .by_group = T) %>%
+  mutate(long_diff = lon - lag(lon),
+         lat_diff = lat - lag(lat),
+         prev_lon = lag(lon),
+         prev_lat = lag(lat),
+         prev_datetime = lag(datetime)) %>%
   as_tibble()
 
 ships_final <- ships_dt %>%
   drop_na(lat_diff, long_diff) %>%
   # filter(lat_diff !=0 & long_diff !=0) %>% # faster but excludes ships that don't move
-  mutate(dist = distCosine(cbind(LON,LAT), cbind(prev_lon,prev_lat))) %>%
-  group_by(SHIPNAME) %>% # remove port grouping
-  arrange(desc(dist), desc(DATETIME)) %>%
+  mutate(dist = distCosine(cbind(lon,lat), cbind(prev_lon,prev_lat))) %>%
+  group_by(shipname) %>% # remove port grouping
+  arrange(desc(dist), desc(datetime)) %>%
   slice_head()
 
 write_csv(ships_final, "data/ships_processed.csv")
