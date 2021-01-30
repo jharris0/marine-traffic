cb5efec9be98a7f48a2da754ec2f6d55e2f38c4b
Jesse Harris <jesserharris@icloud.com>
2021-01-29 17:21:44 -0700

    Memory errors when trying to load 400MB CSV file in Shinyapps.io (1 GB max memory instance), so I am pre-processing the CSV and only using the processed version (<300KB) in the app

--- app.R
+++ app.R
@@ -9,25 +9,7 @@ library(tidyr)
 library(dtplyr)
 library(dplyr, warn.conflicts = F)
 
-ships_raw <- read_csv("data/ships.csv")
-
-ships_dt <- lazy_dt(ships_raw) %>%
-  group_by(SHIPNAME, PORT) %>%
-  arrange(DATETIME, .by_group = T) %>%
-  mutate(long_diff = LON - lag(LON),
-         lat_diff = LAT - lag(LAT),
-         prev_lon = lag(LON),
-         prev_lat = lag(LAT),
-         prev_datetime = lag(DATETIME)) %>%
-  as_tibble()
-
-ships_final <- ships_dt %>%
-  drop_na(lat_diff, long_diff) %>%
-  # filter(lat_diff !=0 & long_diff !=0) %>% # faster but excludes ships that don't move
-  mutate(dist = distCosine(cbind(LON,LAT), cbind(prev_lon,prev_lat))) %>%
-  group_by(SHIPNAME) %>% # remove port grouping
-  arrange(desc(dist), desc(DATETIME)) %>%
-  slice_head()
+ships <- read_csv("data/ships_processed.csv")
 
 ui <- semanticPage(
   title = "Marine Traffic Analyzer",
