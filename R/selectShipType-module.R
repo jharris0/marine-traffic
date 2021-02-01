d0609da947b95662919ea733310fe7f1c67f28c4
Jesse Harris <jesserharris@icloud.com>
2021-01-31 19:49:21 -0700

    Delete various unused modules and example code

--- R/selectShipType-module.R
+++
@@ -1,9 +0,0 @@
-# selectShipTypeUI <- function(id, label = "Ship Type Dropdown") {
-#   ns <- NS(id)
-#   tagList(
-#     dropdown_input(
-#       input_id = "ship_type",
-#       choices = sort(unique(ships()$ship_type))
-#       )
-#   )
-# }
